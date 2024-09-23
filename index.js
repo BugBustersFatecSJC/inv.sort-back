e46083d93e3875e3bf57729acddfab4e9785ec3f
João Suzuki <joaosuzuki98@outlook.com>
2024-09-22 23:57:21 -0300

    feat(index): criação do servidor em node

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-ECHO esta ativado.
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Teste');
+})
+
+// Criação do servidor
+app.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`)
+})
