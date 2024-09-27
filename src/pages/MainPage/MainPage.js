bdb8c20979561e2415bd41c6ca62441c19b04241
Pedro Prevides <pedrologin13@gmail.com>
2024-09-27 20:52:58 -0300

    feat(src): Adicionado componente de carregamento

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import UserProfileIcon from '../../components/UserProfileIcon/UserProfileIcon'
-import Field from '../../components/Field/Field'
-import SendButton from '../../components/SendButton/SendButton'
+import {useState} from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import UserProfileIcon from '../../components/UserProfileIcon/UserProfileIcon';
+import SendButton from '../../components/SendButton/SendButton';
+import Loading from '../../components/Loading/Loading';
 
 function MainPage() {
+  const [loading, setloading] = useState(false);
+  const loadingIcon = () => {
+    setloading((prevLoading) => !prevLoading);
+  };
+
   return (
     <div className='main-color-bg flex'>
       <Sidebar />
@@ -13,13 +18,16 @@ function MainPage() {
         <div className='flex flex-col w-full items-end justify-start p-4'>
           <UserProfileIcon />
         </div>
-        
+
         <div className='w-full flex justify-center'>
-          <SendButton text="ENVIAR" />
+          <button onClick={loadingIcon}>Testing</button>
+        </div>
+        <div className='w-full flex justify-center'>
+          {loading ? <Loading /> : <p>Não está carregando...</p>}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
