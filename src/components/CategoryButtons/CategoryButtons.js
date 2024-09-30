1f252023e63852629dae6d59b69f5aba970fa148
João Suzuki <joaosuzuki98@outlook.com>
2024-09-29 21:26:42 -0300

    style(CategoryButtons): Adicionado borda nos botões

--- src/components/CategoryButtons/CategoryButtons.js
+++ src/components/CategoryButtons/CategoryButtons.js
@@ -3,16 +3,16 @@ import React from 'react';
 function CategoryButtons() {
   return (
     <div>
-      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel mb-1 text-2xl">
+      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel mb-1 text-2xl main-color border-4 border-black">
         Usuários
       </button>
-      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel mb-1 text-2xl">
+      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel mb-1 text-2xl main-color border-4 border-black">
         Produtos
       </button>
-      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel mb-1 text-2xl">
+      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel mb-1 text-2xl main-color border-4 border-black">
         Histórico
       </button>
-      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel text-2xl">
+      <button className="w-full p-2 alt-color-5-bg text-black shadow-lg font-pixel text-2xl main-color border-4 border-black">
         Análise
       </button>
     </div>
