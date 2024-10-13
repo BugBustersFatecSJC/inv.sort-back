167de318cde9c2386579398f9aeb6e26d40d5f05
victordanielrb <victordanielramosb@gmail.com>
2024-10-12 21:05:24 -0300

    [feat] : Criação  da página analytics e união dos componentes

--- src/pages/Analytics/Analytics.js
+++ src/pages/Analytics/Analytics.js
@@ -5,6 +5,8 @@ import Loading from '../../components/Loading/Loading';
 import CategoryButtons from '../../components/CategoryButtons/CategoryButtons';
 import Mainchart from '../../components/MainChart/Mainchart';
 import Sectorchart from '../../components/Sectorchart/Sectorchart'
+import Cardsanalytics from '../../components/Cardsanlaytics/Cardsanalytics';
+import Analyticsboard from '../../components/Analyticsboard/Analyticsboard';
 function Analytics(props) {
   const [loading, setloading] = useState(false);
   const loadingIcon = () => {
@@ -15,24 +17,34 @@ function Analytics(props) {
     <div className='main-color-bg flex'>
       
       <Sidebar />
-
-      <div className='w-full flex overflow-auto flex flex-col items-center'>
+     
+      <div id='main' className='w-full px-12 h-[100%] flex overflow-scroll overflow-x-hidden flex-wrap flex-col items-center'>
       
         <div className='flex flex-col w-full items-end justify-start p-4'>
           <UserProfileIcon />
         </div>
         
-        <div className='w-[100%] h-[50%] flex mx-auto px-8 flex grid grid-cols-3 gap-4'>
-        <div className='flex-auto justify-center m-auto col-span-2 w-[100%] h-[50%]'>
-        <Mainchart />
-        </div>
-        <div className=' flex flex-1  m-auto w-[100%] h-[100%] justify-center text-center'>
-        <Sectorchart />  
+        <Cardsanalytics/>
+       
+        <div className='w-[100%] h-[250px] overflow-x-hidden flex mx-auto  justify-center text-center align-center  '>
+            <div className='h-[100%] px-2 py-2 flex items-center self-center justify-between w-[100%] border-4 rounded border-[#B45105] bg-[#FFC376]'>
+              <Mainchart />
+              </div>       
+              
 
         </div>
+        
+          <div className='min-h-[50px] flex'>
+            
+          </div>
+
+        <div className='h-[100%]  mx-12 px-2 py-2 flex items-center self-center justify-between w-[100%] border-4 rounded border-[#B45105] bg-[#FFC376]'>
+        <Sectorchart />
+        
+          </div>  
         </div>
       </div>
-    </div>
+    
   );
 }
 
