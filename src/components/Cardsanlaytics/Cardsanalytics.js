4e1b061a047a81f11d90c34c49f69b005678ade4
victordanielrb <victordanielramosb@gmail.com>
2024-10-12 21:02:43 -0300

    [feat] : Criação componente da página analytics (Cards complementares)

---
+++ src/components/Cardsanlaytics/Cardsanalytics.js
@@ -0,0 +1,53 @@
+
+const Cardsanalytics = () => {
+
+
+    return (
+        <  ><div className='px-12 mt-2 w-[100%] h-[40px] flex justify-between text-center align-center'>
+        <span className="flex flex-col ">
+    
+            <div className="h-[40px] px-2 flex items-center self-center justify-between w-[200px] border-4 rounded border-[#B45105] bg-[#FFC376] ">
+                <p className="align-middle poppins-semibold text-[#3E1900]">Produto</p>
+                <p className="text-sm poppins-bold text-[#2F5F09]">+16%</p>
+            </div>
+
+        </span>
+  
+    <span className="flex flex-col">
+        
+            <div className="h-[40px] flex px-2 items-center self-center justify-between w-[220px]  border-4 rounded border-[#B45105] bg-[#FFC376] ">
+                <p className=" align-middle poppins-semibold text-[#3E1900]">Produto</p>
+                <p className=" text-sm poppins-bold text-[#2F5F09]">96 un.</p>
+            </div>
+
+    </span>
+    
+    <span className="flex flex-col">
+    
+        <div className="h-[40px] px-2 flex items-center self-center justify-between w-[220px]  border-4 rounded border-[#B45105] bg-[#FFC376] ">
+            <p className=" align-middle poppins-semibold text-[#3E1900]">Categoria</p>
+            <p className="text-sm  poppins-bold text-[#2F5F09]">+16%</p>
+        </div>
+
+    </span>
+    
+    <span className="flex flex-col">
+  
+    <div className="h-[40px] px-2 flex items-center self-center justify-between w-[220px]  border-4 rounded border-[#B45105] bg-[#FFC376] ">
+        <p className=" align-middle poppins-semibold text-[#3E1900]">Categoria</p>
+        <p className=" text-sm poppins-bold text-[#2F5F09]">4907 un.</p>
+    </div>
+
+  </span>
+ 
+    </div>
+     <div className="flex justify-start w-full px-12 m-1 mb-4">
+     <p className="text-[10px]" >Dados referentes ao mês anterior</p>
+     </div>
+    </>
+
+    )
+}
+
+
+export default Cardsanalytics;
