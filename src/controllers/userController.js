258ba2f60bc10752f1e135b8872c5d71fab81121
Carlos Intrieri <carlos.intrieri@fatec.sp.gov.br>
2024-10-14 21:51:31 -0300

    [docs] Documenta código de cadastro e login no backend

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,11 +2,13 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const { hashSync, compareSync } = require('bcrypt');
 
+// Registra dados do usuário
 const createUser = async (req, res) => {
     const { username, email, password, role, status } = req.body;
 
     try {
 
+        // Verifica se o e-mail já está em uso
         const existingUser = await prisma.user.findUnique({
             where: { email: email },
         })
@@ -21,6 +23,7 @@ const createUser = async (req, res) => {
 
         const hashedPassword = hashSync(password, 10);
 
+        // Criação um novo usuário com a senha hasheada
         let user = await prisma.user.create({
             data: {
                 username: username,
@@ -31,8 +34,8 @@ const createUser = async (req, res) => {
             },
         });
 
-        
-        const { password: _, ...userWithoutPassword } = user; 
+        // Retorne o usuário criado com a senha hasheada
+        const { password: _, ...userWithoutPassword } = user; // Remove a senha da resposta
         return res.status(201).json(userWithoutPassword);
     } catch (error) {
         console.log(error)
@@ -41,7 +44,7 @@ const createUser = async (req, res) => {
 };
 
 
-
+// Retorna todos os usuários
 const getAllUsers = async (req, res) => {
     try {
         const users = await prisma.user.findMany();
@@ -55,12 +58,12 @@ const getAllUsers = async (req, res) => {
     }
 };
 
-
+// Função para autenticar um usuário (login)
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-   
+        // Verifica se o usuário existe
         const user = await prisma.user.findUnique({
             where: { email: email },
         });
@@ -69,13 +72,14 @@ const loginUser = async (req, res) => {
             return res.status(401).json({ error: "E-mail ou senha inválidos." });
         }
 
-       
+        // Compara a senha fornecida com a senha hasheada no banco de dados
         const isPasswordValid = compareSync(password, user.password);
         if (!isPasswordValid) {
             return res.status(401).json({ error: "E-mail ou senha inválidos." });
         }
 
-        const { password: _, ...userWithoutPassword } = user;
+        // Retorne o usuário, sem a senha
+        const { password: _, ...userWithoutPassword } = user; // Remove a senha da resposta
         return res.status(200).json(userWithoutPassword);
     } catch (error) {
         console.error(error);
@@ -87,7 +91,6 @@ const loginUser = async (req, res) => {
 module.exports = {
     createUser,
     getAllUsers,
-    loginUser, 
+    loginUser, // Adicione esta linha
 };
-
 
