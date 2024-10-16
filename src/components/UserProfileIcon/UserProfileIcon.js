e78e4129ac2f787888130c5be57635dba31e61b5
danprsp <daniel.piloto@fatec.sp.gov.br>
2024-10-16 12:13:58 -0300

    [feat]:adiciona o login XD

--- src/components/UserProfileIcon/UserProfileIcon.js
+++ src/components/UserProfileIcon/UserProfileIcon.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function UserProfileIcon() {
+  
+  const user = localStorage.getItem("user")
+  const jsonUser = JSON.parse(user)
   return (
     <div className='flex items-center'>
         <p className='font-pixel text-xl me-3'>
-            Usuário
+            {jsonUser.username}
         </p>
         <figure className='bg-white rounded-full w-[4rem] h-[4rem]'>
         
