8ce674358b52d6db548dd750acc149e6adc2b3bc
victordanielrb <victordanielramosb@gmail.com>
2024-10-20 14:30:39 -0300

    [feat] Alterado estilo dos fundos dos gráficos

--- src/pages/Analytics/Analytics.js
+++ src/pages/Analytics/Analytics.js
@@ -5,8 +5,6 @@ import Loading from '../../components/Loading/Loading';
 import CategoryButtons from '../../components/CategoryButtons/CategoryButtons';
 import Mainchart from '../../components/MainChart/Mainchart';
 import Sectorchart from '../../components/Sectorchart/Sectorchart'
-import Cardsanalytics from '../../components/Cardsanlaytics/Cardsanalytics';
-import MainPage from '../MainPage/MainPage';
 
 
 function Analytics(props) {
@@ -16,21 +14,34 @@ function Analytics(props) {
   };
   
   return (
-    <MainPage title="Análise dos itens">
-      <Cardsanalytics/>
+    <div className='main-color-bg h-[100%] flex'>
       
-      <div className='w-[100%] h-[100%] mt-12 grid grid-cols-3 gap-6'>
-        <div className='col-span-2 flex flex-col  h-[100%] px-2 py-2  flex justify-center border-4 rounded border-[#B45105] bg-[#FFC376]'>
-          {/* <h1 className='poppins-semibold'></h1> */}
+      <Sidebar />
+     
+      <div id='main' className='w-[100%] min-h-[100vh] px-12 h-[100%] grid-col-2 flex overflow-scroll overflow-x-hidden flex-wrap flex-col  text-center justify-start items-center'>
+
+        <div className='flex flex-col w-full items-end justify-start p-4'>
+        <UserProfileIcon/>
+y
+        </div>
+        
+      
+       
+        <div className='w-[100%] h-[100%] mt-12 grid grid-cols-3 gap-6'>
+        <div className='col-span-2 flex flex-col  h-[100%] px-2 py-2  flex justify-center border-4   border-[rgb(180,81,5)] outline outline-4 outline-[rgb(107,55,16)] bg-[#FFC376]'>
+          <h1 className='poppins-semibold'></h1>
+          
           <Mainchart />
         </div>
 
-        <div className='h-[100%] col-span-1 px-2 py-2 flex flex-col items-center self-center w-[100%] border-4 rounded border-[#B45105] bg-[#FFC376]'>
+        <div className='h-[100%] col-span-1 px-2 py-2 flex flex-col items-center self-center w-[100%] border-4   border-[rgb(180,81,5)] outline outline-4 outline-[rgb(107,55,16)] bg-[#FFC376]'>
         <h1 className='poppins-semibold'>Vendas por Categorias </h1>
           <Sectorchart />
         </div>
       </div>
-    </MainPage>  
+            </div>
+      </div>
+    
   );
 }
 
