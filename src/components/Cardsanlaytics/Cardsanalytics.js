d2dc7f30379054d23953ddc22bf8df3154b19c94
victordanielrb <victordanielramosb@gmail.com>
2024-11-06 18:21:34 -0300

    [feat]Cria os cards de informação na página analytics

---
+++ src/components/Cardsanlaytics/Cardsanalytics.js
@@ -0,0 +1,24 @@
+import api from '../../services/api';
+import { useState, useEffect } from 'react';
+const Cardsanalytics = ({text,label,data,datacor,prefix='',sufix=''}) => {
+
+    // Iterated from: https://stackoverflow.com/questions/10599933/convert-long-number-into-abbreviated-string-in-javascript-with-a-special-shortn
+    
+    return (
+        
+       
+            <span className="flex w-full    my-2 flex-col ">
+            <h1 className='poppins-medium text-start text-[12px] text-[#6B3710] w-full px-1'>{text}</h1>
+            <div className="h-[40px] w-full pl-1 align-middle flex items-center self-center justify-between  border-4 rounded border-[#B45105] bg-[#FFC376] ">
+                <p className="align-middle poppins-semibold text-[#6B3710]">{label}</p>
+                <p className={`text-sm poppins-bold bg-[#B45105] text-[#3E1900] tracking-wider p-1 content-center  my-auto h-full text-${datacor}`}>{prefix+data+sufix}</p>
+            </div>
+
+        </span>
+       
+  
+    )
+}
+
+
+export default Cardsanalytics;
