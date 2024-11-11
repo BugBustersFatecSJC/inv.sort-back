16d3aa23655d56830016916a16bec334d79b0e22
Gabriel Guimarães <biel_google@hotmail.com>
2024-11-10 21:24:34 -0300

    [fix]solving dropdown buttons conflicts

--- src/components/DropdownButtons/DropdownButtons.js
+++ src/components/DropdownButtons/DropdownButtons.js
@@ -1,5 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
+
 import { useState, useEffect } from 'react'
 import api from '../../services/api'
 
@@ -24,20 +24,12 @@ function DropdownButtons({ options = [],type, label, onChange, disabled = false,
     <div className=' mx-2 w-full'>
       <select
         className={`${windowWidth >500?'w-[8rem]': 'w-[90%]'} poppins-semibold border-4 rounded border-[#B45105] bg-[#FFC376] animated-placeholder`}
-=======
 
-function DropdownButtons({ options = [],type, label, onChange, disabled = false }) {
-
-  return (
-    <div className=' ml-4'>
-      <select
-        className="w-36 poppins-semibold border-4 rounded border-[#B45105] bg-[#FFC376]"
->>>>>>> 1f76bc0f7120bf7f5b9fe98df825ebb54d1e6c90
         name="dropdown"
         id="filtrodrop"
         onChange={onChange}
         disabled={disabled}
-<<<<<<< HEAD
+
         
       >
         {padrao === true ? <option value="null"  >{label}</option> : ''}
@@ -49,14 +41,7 @@ function DropdownButtons({ options = [],type, label, onChange, disabled = false 
             {option[`${type}_name`]}
           </option>
           </>
-=======
-      >
-        <option value="null">{label}</option>
-        {options.map((option) => (
-          <option key={option[`${type}_id`]} value={option[`${type}_id`]}>
-            {option[`${type}_name`]}
-          </option>
->>>>>>> 1f76bc0f7120bf7f5b9fe98df825ebb54d1e6c90
+
         ))}
       </select>
     </div>
